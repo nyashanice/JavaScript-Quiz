f002fcbb1eac6db85cd979f69dfb268cf6accc67
Nya Tyus <nyastyus@gmail.com>
2023-01-24 21:20:52 -0500

    questions are now iterating through array when answered correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,55 +6,73 @@
 // Save high score and initials at end
 var questionsArr = [
     {
-        prompt: "What's is my name?",
+        prompt: "What is my name?",
         choices: ["Nya", "Tomato", "Potato", "Cheese"],
         correctAnswer: "Nya"
     },
     {
-        prompt: "What's is my last?",
+        prompt: "What is my last?",
         choices: ["Tyus", "Acovado", "Lime", "Orange"],
         correctAnswer: "Tyus"
-    }, {
+    },
+    {
         prompt: "How old am I?",
         choices: [23, 22, 21, 20],
         correctAnswer: 23
     }
 ]
 
-var questionIndex;
+var questionIndex = 0;
 var timeRem = document.getElementById("time-remaining");
 var secondsRemaining = 60;
 var startButton = document.querySelector(".start-button");
+var currentQuestion;
+var currentPrompt;
+var rightAnswer;
+// var choiceButton = document.createElement("button");
 
 // starts the 60 second timer and displays on screen 
 function quizTimer() {
     var interval = setInterval(function () {
-        console.log("timer")
+        // console.log("timer")
         secondsRemaining--;
         timeRem.textContent = "Time remaining: " + secondsRemaining;
 
         if (secondsRemaining === 0) {
             clearInterval(interval);
-            endQuiz()
+            endQuiz();
         }
         // decrease in increments of 1 s or 1000 ms
     }, 1000);
 }
 
+// Starts quiz by calling the showQuestion function and hides start button
 function start(event) {
     event.preventDefault();
     quizTimer();
-    questionIndex = 0;
+    // questionIndex = 0;
     startButton.style.display = "none";
     showQuestion();
 }
 
+// Displays quiz questions on screen
 function showQuestion() {
-    console.log("Showing question");
-    var currentQuestion = questionsArr[questionIndex];
-    var currentPrompt = currentQuestion.prompt;
+    // console.log("Showing question");
+    currentQuestion = questionsArr[questionIndex];
+    // console.log(currentQuestion);
+    currentPrompt = currentQuestion.prompt;
+    rightAnswer = currentQuestion.correctAnswer;
     document.getElementById("quizQuestions").innerHTML = currentPrompt;
-    console.log(currentPrompt)
+    // console.log(currentPrompt)
+    for (var i = 0; i < questionsArr.length; i++) {
+        console.log(currentPrompt)
+        questionDisplay = (i + 1) + ". " + currentPrompt;
+        console.log(questionDisplay);
+        // questionIndex++;
+        console.log(questionIndex);
+        console.log(currentQuestion);
+
+    }
     var questionChoices = currentQuestion.choices;
     for (var i = 0; i < questionChoices.length; i++) {
         var choiceButton = document.createElement("button");
@@ -62,6 +80,8 @@ function showQuestion() {
         choiceButton.textContent = questionChoices[i];
         document.body.appendChild(choiceButton);
         console.log(choiceButton)
+        // checkAnswer();
+
     }
 
     // how do I know what question I'm at?
@@ -71,10 +91,21 @@ function showQuestion() {
 
 
 function checkAnswer(event) {
+    // var rightAnswer = currentQuestion.correctAnswer;
+    console.log(rightAnswer, event.target.textContent);
+    if (event.target.textContent == rightAnswer) {
+        alert('Correct!');
+        questionIndex++;
+        showQuestion();
+    } else {
+        alert('Not quite');
+    }
+
     event.preventDefault();
     console.log(event);
     console.log(this);
     console.log(event.target);
+
 
 
     // Check if you're done answering all the questions
