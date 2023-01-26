30f8ac1aff44eb0ceb20ae97c2705ee4ad266c4b
Nya Tyus <nyastyus@gmail.com>
2023-01-26 10:20:03 -0500

    quiz ends when all questions are answered or timer runs out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-// Click start button $
-// Timer starts on click $
-// Question pops up
-// Wrong answers takes away time from clock
-// Game ends at 0
-// Save high score and initials at end
+// declaring all variables
+var questionsArr;
+var interval;
+var timeRem = document.getElementById("time-remaining");
+var secondsRemaining = 60;
+var startButton = document.querySelector(".start-button");
+var currentQuestion;
+var currentPrompt;
+var questionChoices;
+var choiceButton;
+var rightAnswer;
+var questionIndex = 0;
+var score = 0;
+var buttonHolder;
+var initials;
+var user;
+var storage;
+var tryAgain = document.querySelector(".tryAgainButton");
 
-
-var questionsArr = [
+// storing questions with a question prompt, answer choices to choose from, and a correct answer 
+questionsArr = [
     {
         prompt: "What is my name?",
         choices: ["Nya", "Tomato", "Potato", "Cheese"],
@@ -24,83 +36,60 @@ var questionsArr = [
     }
 ]
 
-var questionIndex = 0;
-var timeRem = document.getElementById("time-remaining");
-var secondsRemaining = 60;
-var startButton = document.querySelector(".start-button");
-var currentQuestion;
-var currentPrompt;
-var rightAnswer;
-var score = 0;
-var highscoreForm = document.getElementById('highscoreName');
-score = document.getElementById('endScore');
-var highscoreName = document.getElementById('name');
-var buttonHolder;
+// hides try again button at start of quiz
+tryAgain.style.display = 'none';
 
-// starts the 60 second timer and displays on screen 
-function quizTimer() {
-    var interval = setInterval(function () {
-        // console.log("timer")
-        secondsRemaining--;
-        timeRem.textContent = "Time remaining: " + secondsRemaining;
-
-        if (secondsRemaining === 0) {
-            clearInterval(interval);
-            endQuiz();
-        }
-        // decrease in increments of 1 s or 1000 ms
-    }, 1000);
-}
-
-// Starts quiz by calling the showQuestion function and hides start button
+startButton.addEventListener("click", start);
+// Starts quiz with timer and questions
 function start(event) {
     event.preventDefault();
     quizTimer();
-    // questionIndex = 0;
     startButton.style.display = "none";
     showQuestion();
 }
 
-// Displays quiz questions on screen
+// starts the 60 second timer and displays on screen 
+function quizTimer() {
+    interval = setInterval(function () {
+        secondsRemaining--;
+        timeRem.textContent = "Time remaining: " + secondsRemaining;
+
+        if (secondsRemaining === 0) {
+            endQuiz();
+        }
+        // decrement of 1 s or 1000 ms
+    }, 1000);
+}
+
+// Displays numbered quiz questions on screen along with all answer choices for that question
 function showQuestion() {
-    // console.log("Showing question");
     currentQuestion = questionsArr[questionIndex];
-    // console.log(currentQuestion);
     currentPrompt = currentQuestion.prompt;
     rightAnswer = currentQuestion.correctAnswer;
     document.getElementById("quizQuestions").innerHTML = (questionIndex + 1) + ": " + currentPrompt;
-    // console.log(currentPrompt)
 
-    var questionChoices = currentQuestion.choices;
-    //where are those buttons going to go??
+    questionChoices = currentQuestion.choices;
     buttonHolder = document.getElementById("quiz-choices");
-    //if there's anything there, should I clear it before I append them?
-    buttonHolder.innerHTML = ""
+    buttonHolder.innerHTML = "";
 
+    // displays appropriate answer choices as the question changes
     for (var i = 0; i < questionChoices.length; i++) {
-        var choiceButton = document.createElement("button");
+        choiceButton = document.createElement("button");
         choiceButton.addEventListener("click", checkAnswer);
         choiceButton.textContent = questionChoices[i];
         buttonHolder.appendChild(choiceButton);
-        console.log(choiceButton)
-        // checkAnswer();
-
     }
 
-    // how do I know what question I'm at?
-    //Do I want to iterate through all my questions?
-    //If I do a for loop, how will my code act?
 }
 
-
+// checks to see if chosen answer is right or wrong
 function checkAnswer(event) {
-    // var rightAnswer = currentQuestion.correctAnswer;
-    console.log(rightAnswer, event.target.textContent);
+
     if (event.target.textContent == rightAnswer) {
         alert('Correct!');
         score += 20;
-        //        localStorage.setItem('score', score);
         questionIndex++;
+        // uses question index to determine if all questions have been answered before timer runs out
         if (questionIndex == questionsArr.length) {
             endQuiz();
         } else {
@@ -109,69 +98,65 @@ function checkAnswer(event) {
     } else {
         alert('Not quite');
         questionIndex++;
-        if (secondsRemaining > 10) {
-            secondsRemaining -= 10;
-            showQuestion();
-        } else {
+        if (questionIndex == questionsArr.length) {
             endQuiz();
+        } else {
+            // takes away time from quiz timer if there is enough time remaining
+            if (secondsRemaining > 10) {
+                secondsRemaining -= 10;
+                showQuestion();
+            } else {
+                endQuiz();
+            }
         }
-    }
 
+    }
     event.preventDefault();
     console.log(event);
     console.log(this);
     console.log(event.target);
-
-
-
-    // Check if you're done answering all the questions
-    // if statement
-    // edit the questionIndex after we check it
 }
-startButton.addEventListener("click", start);
 
+// what happens with elements and data once quiz ends
 function endQuiz() {
     console.log("Ending!")
-    // currentPrompt.style.display = 'none';
+    clearInterval(interval);
+    timeRem.style.display = 'none';
     buttonHolder.innerHTML = '';
     document.getElementById('quizQuestions').innerHTML = '';
-    // score.style.display = 'block';
-    // highscoreForm.style.display = 'block';
-    // JSON.stringify(highscoreName);
-    // JSON.stringify(score);
-    // var initials = document.getElementById('initials').value;
-    // console.log(initials);
-    var initials = prompt('Enter your initials to be entered into the highscores list!');
-    console.log(initials);
-    var user = {
+    tryAgain.style.display = 'block';
+    initials = prompt('Enter your initials to be entered into the highscores list!');
+    user = {
         initials: initials,
         points: score
     };
+    console.log(user);
     saveStorage(user);
-    //Once the user submits their initials, we get the values
-    //var user = { initials: "Value",
-    //points: score}
-    // saveStorage(user)
-    // localStorage.setItem('highscores', [highscoreName, score]);
+    localStorage.setItem("user", JSON.stringify([user]));
 }
 
+// saves each new score in local storage
 function saveStorage(newScore) {
-    var storage = JSON.parse(localStorage.getItem("highscores"))
-    storage.push(newScore)
-    localStorage.setItem("highscores", JSON.stringify(storage))
-
+    // ????????????? can't add additional scores
+    storage = JSON.parse(localStorage.getItem("user"));
+    storage.push(newScore);
+    localStorage.setItem("user", JSON.stringify(storage));
 }
 
+// 
 function loadStorage() {
-    var storage = JSON.parse(localStorage.getItem("highscores"))
+    storage = JSON.parse(localStorage.getItem("user"));
     console.log(storage)
     if (!storage) {
-        console.log("NO Storage exists!!!")
-        localStorage.setItem("highscores", JSON.stringify([]))
-        return
+        console.log("NO Storage exists!!!");
+        localStorage.setItem("user", JSON.stringify([]));
+        return;
     }
-    //render that storage!!
+}
+
+// ????????????? what does this do
+function renderHighscores() {
 
 }
 
-loadStorage()
+loadStorage();
