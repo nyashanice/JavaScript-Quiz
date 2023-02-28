012dbdee32e76bb0e2af91fcf45b887855eb7275
Nya Tyus <nyastyus@gmail.com>
2023-02-27 22:10:59 -0500

    remove console logs from javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // declaring all variables
 var timeRem = document.getElementById("time-remaining");
 var startButton = document.querySelector(".start-button");
-var tryAgain = document.querySelector(".tryAgainButton");
 var quizDescription = document.getElementById("descriptionText");
 
 var secondsRemaining = 60;
@@ -42,9 +41,6 @@ questionsArr = [
     correctAnswer: 23,
   },
 ];
-
-// hides try again button at start of quiz
-tryAgain.style.display = "none";
 
 startButton.addEventListener("click", start);
 // Starts quiz with timer and questions
@@ -128,48 +124,34 @@ function checkAnswer(event) {
     }
   }
   event.preventDefault();
-  console.log(event);
-  console.log(this);
-  console.log(event.target);
 }
 
 // what happens with elements and data once quiz ends
 function endQuiz() {
-  console.log("Ending!");
   clearInterval(interval);
   timeRem.style.display = "none";
   buttonHolder.innerHTML = "";
   document.getElementById("quizQuestions").innerHTML = "";
-  tryAgain.style.display = "block";
   initials = prompt(
     "Enter your initials to be entered into the highscores list!"
   );
-  // user = [];
   userData = {
     initials: initials,
     points: score,
   };
-  // console.log(typeof user);
-  console.log(userData);
   saveStorage(userData);
-  // localStorage.setItem("user", JSON.stringify([userData]));
 }
 
 // saves each new score in local storage
 function saveStorage(newScore) {
-  // ????????????? can't add additional scores
   storage = JSON.parse(localStorage.getItem("user"));
-  // storage = localStorage.getItem(JSON.parse(user));
   newUserData = newScore;
-  // console.log(typeof storage);
   storage.push(newUserData);
-  console.log(storage);
   localStorage.setItem("user", JSON.stringify(storage));
 }
 
 function loadStorage() {
   storage = JSON.parse(localStorage.getItem("user"));
-  console.log(storage);
   if (!storage) {
     console.log("NO Storage exists!!!");
     localStorage.setItem("user", JSON.stringify([]));
@@ -177,14 +159,10 @@ function loadStorage() {
   }
 }
 
-// ????????????? what does this do
 function renderHighscores() {
   storage = JSON.parse(localStorage.getItem("user"));
-  console.log(storage);
   if (storage !== null) {
-    console.log(storage[storage.length - 1].initials);
     var lastInitials = storage[storage.length - 1].initials;
-    console.log(storage[storage.length - 1].points);
     var lastScore = storage[storage.length - 1].points;
     document.getElementById("endScore").innerHTML =
       lastInitials + " got a score of " + lastScore;
@@ -203,16 +181,13 @@ function viewHighscores(event) {
     highscoresDisplay = document.getElementById("highscoresList");
     highscoresDisplay.style.display = "block";
     storageContent = JSON.parse(localStorage.getItem("user"));
-    console.log(storageContent);
     storage = document.createElement("div");
     for (i = 0; i < storageContent.length; i++) {
       storage.textContent =
         storageContent[i].initials + ": " + storageContent[i].points;
       pointsArr.push(storage.textContent);
-      console.log(pointsArr);
     }
     storage.textContent = JSON.stringify(pointsArr);
-    console.log(storage);
     highscoresDisplay.appendChild(storage);
   }
 }
